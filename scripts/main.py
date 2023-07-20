d7a9a62d5fdd462846ba9a364deeaaf3d6ec3b55
giacomo <giacomofranchini@polito.it>
2023-07-20 18:44:51 +0200

    refactor to accept config file from cmd line

---
+++ scripts/main.py
@@ -0,0 +1,34 @@
+from omni.isaac.kit import SimulationApp
+import argparse
+import yaml
+
+parser = argparse.ArgumentParser(description="Launch IsaacSim and spawn robots")
+parser.add_argument(
+    "--config-file",
+    action="store",
+    help='Config file relative path',
+    default='../config/jackal_vigna.yaml',  
+)
+args, unknown = parser.parse_known_args()
+
+with open(args.config_file, "r") as file:
+    config_file = yaml.safe_load(file)
+    stage_config = config_file.get("stage")
+    robot_config = config_file.get("robots")
+
+sim_app = SimulationApp()
+
+from isaac_sim_ros import IsaacRos
+
+def main():
+    isaac_ros_simulation = IsaacRos(sim_app)
+    isaac_ros_simulation.add_default_ground_plane()
+    isaac_ros_simulation.add_usd_to_stage(stage_config.get("usd_path"), stage_config.get("root_prim_path"))
+    isaac_ros_simulation.spawn_robots(robot_config)
+    isaac_ros_simulation.start()
+    while sim_app._app.is_running() and not sim_app.is_exiting():
+        sim_app.update()
+    sim_app.close()
+
+if __name__ == "__main__":
+    main()
